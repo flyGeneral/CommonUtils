fe117efe6f04164f45599d4db41d3ecbc277687d
flyGeneral <479736308@qq.com>
2017-04-05 20:43:03 +0800

    先修改一波名字

---
+++ imagesdk/src/main/java/com/chenjinghao/imagesdk/ImageLoaderManager.java
@@ -0,0 +1,14 @@
+package com.chenjinghao.imagesdk;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+/**
+ * 初始化UniverImagerLoader，并用来加载图片
+ * Created by chenjinghao on 2017/4/5.
+ */
+
+public class ImageLoaderManager {
+
+}
