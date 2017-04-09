736ba7c43077704082af13f03230960bd58c0894
flyGeneral <479736308@qq.com>
2017-04-09 13:07:24 +0800

    测试扫码功能通过，正常返回扫码结果

--- app/src/main/java/com/chenjinghao/commonutils/MainActivity.java
+++ app/src/main/java/com/chenjinghao/commonutils/MainActivity.java
@@ -1,9 +1,11 @@
 package com.chenjinghao.commonutils;
 
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,28 +17,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private TextView tvInfo;
     private Button mBtnShowPicture;
+    private Button mBtnScanCode;
+    private TextView mTvQCodeResult;
+
+    private static final int REQUEST_QRCODE = 0x01;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvInfo = (TextView) findViewById(R.id.tv_info);
-        mBtnShowPicture = (Button) findViewById(R.id.btn_show_picture);
+        initViews();
 
         tvInfo.setText("内存："+ MemoryManager.getCommonMemoryValue(this)+"---最大内存："+
                 MemoryManager.getLargeMemoryValue(this));
-        mBtnShowPicture.setOnClickListener(this);
-
     }
 
+    private void initViews() {
+        tvInfo = (TextView) findViewById(R.id.tv_info);
+        mBtnShowPicture = (Button) findViewById(R.id.btn_show_picture);
+        mBtnShowPicture.setOnClickListener(this);
+        mBtnScanCode = (Button) findViewById(R.id.btn_scan_q_code);
+        mBtnScanCode.setOnClickListener(this);
+        mTvQCodeResult = (TextView) findViewById(R.id.tv_show_q_code_result);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            //扫码逻辑处理
+            case REQUEST_QRCODE:
+                Log.d("jinghao", "resultCode="+requestCode);
+                if (resultCode == Activity.RESULT_OK){
+                    Log.d("jinghao", "扫码成功");
+                    String scan_result = data.getStringExtra("SCAN_RESULT");
+                    mTvQCodeResult.setText("扫码结果是："+scan_result);
+                }
+                break;
+        }
+    }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btn_show_picture:
-                Intent toShowPicture = new Intent(this, CaptureActivity.class);
+                Intent toShowPicture = new Intent(this, ShowPictureActivity.class);
                 startActivity(toShowPicture);
+                break;
+            case R.id.btn_scan_q_code:
+                Intent toScanCode = new Intent(this, CaptureActivity.class);
+                startActivityForResult(toScanCode, REQUEST_QRCODE);
+//                startActivity(toScanCode);
                 break;
         }
     }
